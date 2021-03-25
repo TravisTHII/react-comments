c1a95332f1e6f9d847fb13cc152214b81e91566c
unknown <travisthackneyii@gmail.com>
2021-03-25 17:52:21 -0400

    New structure

--- client/src/components/includes/Badge.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function Badge({ badge }) {
-	return (
-		<span className="badge" style={{ backgroundColor: `${badge.backgroundColor}`, color: `${badge.textColor}` }}>
-			{badge.title}
-		</span>
-	)
-}
