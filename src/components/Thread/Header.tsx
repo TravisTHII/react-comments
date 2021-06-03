f69ccf74ed8fbaec9c427f281aa1e512b36335df
TravisTHII <travisthackneyii@gmail.com>
2021-06-03 12:11:29 -0400

    Fix: Sort menu

--- src/components/Thread/Header.tsx
+++ src/components/Thread/Header.tsx
@@ -6,40 +6,42 @@ import { useOutsideClick } from 'hooks/useOutsideClick'
 
 export function Header() {
 
-	const { total, sortThread } = useThreadContext()
+  const { total, sortThread } = useThreadContext()
 
-	const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false)
 
-	const clickRef = useOutsideClick(() => {
-		setOpen(false)
-	})
+  const clickRef = useOutsideClick(() => {
+    if (open) {
+      setOpen(false)
+    }
+  })
 
-	return (
-		<div className="header">
-			<div>
-				<p>{`${total} Comment${total > 1 ? 's' : ''}`}</p>
-			</div>
-			<div
-				className="header-menu"
-				ref={clickRef}
-				onClick={() => setOpen(!open)}
-			>
-				<div className="menu-item">Sort</div>
-				<ul className={open ? 'menu-open' : ''}>
-					<li
-						className="menu-item"
-						onClick={() => sortThread('newest')}
-					>
-						Newest
+  return (
+    <div className="header">
+      <div>
+        <p>{`${total} Comment${total > 1 ? 's' : ''}`}</p>
+      </div>
+      <div
+        className="header-menu"
+        ref={clickRef}
+        onClick={() => setOpen(!open)}
+      >
+        <div className="menu-item">Sort</div>
+        <ul className={open ? 'menu-open' : ''}>
+          <li
+            className="menu-item"
+            onClick={() => sortThread('newest')}
+          >
+            Newest
 					</li>
-					<li
-						className="menu-item"
-						onClick={() => sortThread('oldest')}
-					>
-						Oldest
+          <li
+            className="menu-item"
+            onClick={() => sortThread('oldest')}
+          >
+            Oldest
 					</li>
-				</ul>
-			</div>
-		</div>
-	)
+        </ul>
+      </div>
+    </div>
+  )
 }
--- src/components/Thread/Menu.tsx
+++ src/components/Thread/Menu.tsx
@@ -11,96 +11,96 @@ import { MenuProps } from './types'
 
 export function Menu({ deleteRef }: MenuProps) {
 
-	const {
-		thread,
-		menu: {
-			commentRef,
-			data
-		},
-		pinned: {
-			pinned_id
-		},
-		destroyMenu,
-		updatePinnedComment
-	} = useThreadContext()
+  const {
+    thread,
+    menu: {
+      commentRef,
+      data
+    },
+    pinned: {
+      pinned_id
+    },
+    destroyMenu,
+    updatePinnedComment
+  } = useThreadContext()
 
-	const { comment, pinComment, startEditing, deleteComment } = useCommentContext()
+  const { comment, pinComment, startEditing, deleteComment } = useCommentContext()
 
-	const [updatedMenu, setUpdatedMenu] = useState(data)
+  const [updatedMenu, setUpdatedMenu] = useState(data)
 
-	const menuRef = useOutsideClick((e) => {
-		if (!(e.target as Element).closest('.comment_options')) {
-			destroyMenu()
-		}
-	})
+  const menuRef = useOutsideClick((e) => {
+    if (!(e.target as Element).closest('.comment_options')) {
+      destroyMenu()
+    }
+  })
 
-	useEffect(() => {
+  useEffect(() => {
 
-		const copy = [...updatedMenu]
+    const copy = [...updatedMenu]
 
-		let index: number = 0
+    let index: number = 0
 
-		for (const i of copy) {
-			if (/^(Pin|Unpin)$/.test(i)) index = copy.indexOf(i)
-		}
+    for (const i of copy) {
+      if (/^(Pin|Unpin)$/.test(i)) index = copy.indexOf(i)
+    }
 
-		copy[index] = (pinned_id === comment._id) ? 'Unpin' : 'Pin'
+    copy[index] = (pinned_id === comment._id) ? 'Unpin' : 'Pin'
 
-		setUpdatedMenu(copy)
+    setUpdatedMenu(copy)
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-	useEffect(() => {
-		const m = menuRef.current
-			, elemTop = offset(commentRef).top
-			, elemLeft = offset(commentRef).left
-			, elemWidth = parseInt(getComputedStyle(commentRef as Element).width)
+  useEffect(() => {
+    const m = menuRef.current
+      , elemTop = offset(commentRef).top
+      , elemLeft = offset(commentRef).left
+      , elemWidth = parseInt(getComputedStyle(commentRef as Element).width)
 
-		if (m) {
-			m.style.top = `${elemTop + 5}px`
-			m.style.left = `${elemLeft + elemWidth}px`
-		}
-	}, [commentRef, menuRef])
+    if (m) {
+      m.style.top = `${elemTop + 5}px`
+      m.style.left = `${elemLeft + elemWidth}px`
+    }
+  }, [commentRef, menuRef])
 
-	useEffect(() => {
-		window.addEventListener('scroll', destroyMenu)
+  useEffect(() => {
+    window.addEventListener('scroll', destroyMenu)
 
-		return () => window.removeEventListener('scroll', destroyMenu)
-	}, [destroyMenu])
+    return () => window.removeEventListener('scroll', destroyMenu)
+  }, [destroyMenu])
 
-	const menuAction = async (m: string) => {
-		switch (m) {
-			case 'Pin': case 'Unpin':
-				await pinComment(thread)
-				updatePinnedComment(comment._id, m)
-				break;
-			case 'Edit': startEditing(); break;
-			case 'Delete': deleteComment(deleteRef.current); break;
-			case 'Report':
-				alert('Comment reported.\n\nNOTE: Demonstration purposes only, reporting is not fully implemented.');
-				break;
-			default: break;
-		}
-	}
+  const menuAction = async (m: string) => {
+    switch (m) {
+      case 'Pin': case 'Unpin':
+        await pinComment(thread)
+        updatePinnedComment(comment._id, m)
+        break;
+      case 'Edit': startEditing(); break;
+      case 'Delete': deleteComment(deleteRef.current); break;
+      case 'Report':
+        alert('Comment reported.\n\nNOTE: Demonstration purposes only, reporting is not fully implemented.');
+        break;
+      default: break;
+    }
+  }
 
-	return (
-		<span
-			id="menu_render"
-			className="comment_menu"
-			ref={menuRef}
-		>
-			<div className="opt_menu_container">
-				{updatedMenu.map((m, i) => (
-					<div
-						key={i}
-						className="opt_menu_item"
-						onClick={() => menuAction(m)}
-					>
-						{m}
-					</div>
-				))}
-			</div>
-		</span>
-	)
+  return (
+    <span
+      id="menu_render"
+      className="comment_menu"
+      ref={menuRef}
+    >
+      <div className="opt_menu_container">
+        {updatedMenu.map((m, i) => (
+          <div
+            key={i}
+            className="opt_menu_item"
+            onClick={() => menuAction(m)}
+          >
+            {m}
+          </div>
+        ))}
+      </div>
+    </span>
+  )
 }
