10095d9fe4ca2b02a4dd1512730af1c809712ad3
TravisTHII <travisthackneyii@gmail.com>
2021-08-06 16:48:46 -0400

    Update: skeleton loaders

--- src/components/Skeletons/index.tsx
+++ src/components/Skeletons/index.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
-
-import { SelectorSkeleton } from './SelectorSkeleton'
-import { ThreadSkeleton } from './ThreadSkeleton'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 
 export const SkeletonLoader = () => (
-  <>
-    <SelectorSkeleton />
-    <ThreadSkeleton />
-  </>
+  <SkeletonTheme color="#202020" highlightColor="#444">
+    <div className="selectors_skeleton">
+      <div className="select_user_skeleton">
+        <div>
+          <Skeleton className="select_user_header_skeleton" />
+        </div>
+        <div className="select_user_users_skeleton">
+          <Skeleton count={5} className="select_user_square_skeleton" />
+        </div>
+      </div>
+
+      <div className="select_thread_skeleton">
+        <div>
+          <Skeleton className="select_thread_header_skeleton" />
+        </div>
+        <div className="select_thread_threads_skeleton">
+          <Skeleton count={3} className="select_thread_item_skeleton" />
+        </div>
+      </div>
+    </div>
+
+    <div className="thread_column_skeleton">
+      <div className="thread_header_skeleton">
+        <Skeleton className="thread_header_item_skeleton" />
+        <Skeleton className="thread_header_item_skeleton" />
+      </div>
+
+      <div>
+        <Skeleton className="thread_sitc_skeleton" />
+      </div>
+
+      <div className="thread_comment_skeleton">
+        <Skeleton count={9} className="comment_skeleton" />
+      </div>
+    </div>
+  </SkeletonTheme>
 )
